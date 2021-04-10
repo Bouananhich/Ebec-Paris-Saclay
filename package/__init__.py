dda7d8bc44978c4dde8749a13566b5db7e373bec
Arnaud Petit <arnaud.petit39@gmail.com>
2021-04-10 21:08:32 +0200

    new functions

--- package/__init__.py
+++ package/__init__.py
@@ -2,6 +2,7 @@
 from .API.get_nearest_city import get_nearest_city
 from .API.get_nearest_street import get_nearest_street
 from .API.get_ways_from_node import get_ways_from_node
+from .utils.utils import get_ways
 
 __all__ = ["get_nearest_city", "get_nearest_street",
-           "get_ways_from_node"]
+           "get_ways_from_node", "get_ways"]
--- package/utils/utils.py
+++ package/utils/utils.py
@@ -1,5 +1,26 @@
 """Useful functions"""
+from typing import List, Tuple
+from ..API.get_nearest_street import get_nearest_street
+from ..API.get_ways_from_node import get_ways_from_node
 
-def get_x_y():
 
-    return _
+def get_nodes(
+    latitude: float = 48.89535,
+    longitude: float = 2.24697,
+) -> List[int]:
+    """Get nodes near position."""
+    street = get_nearest_street(latitude=latitude,
+                                longitude=longitude)
+    nodes = street.get("nodes", "Error with API")
+    return nodes
+
+
+def get_ways(
+    latitude: float = 48.89535,
+    longitude: float = 2.24697,
+) -> List[Tuple]:
+    """Get ways crossing nearest street of point."""
+    nodes = get_nodes(latitude=latitude,
+                      longitude=longitude)
+    ways = get_ways_from_node(list_node=nodes)
+    return ways
