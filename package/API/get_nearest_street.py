56da5bfe99103c77ac98236e97d153503f49be32
Arnaud Petit <arnaud.petit39@gmail.com>
2021-04-11 18:31:49 +0200

    better binary search

--- package/API/get_nearest_street.py
+++ package/API/get_nearest_street.py
@@ -15,13 +15,14 @@ def get_nearest_street(
     """."""
     #Get hyperparameters from yaml.
     radplus = config.data.get("Nearest_street").get(
-        "Binary_search").get("initial_upper_bound_radius", 10000)
+        "Binary_search").get("initial_upper_bound_radius",100)
     radmoins = config.data.get("Nearest_street").get(
         "Binary_search").get("lower_bound_radius", 0)
     max_iter_before_increased_radius = config.data.get("Nearest_street").get(
         "Binary_search").get("iter_before_increased_radius", 10)
     overpass_url = config.data.get("API").get(
         "overpass_url", "http://overpass-api.de/api/interpreter")
+
     rad = (radplus + radmoins) / 2
     overpass_query = query_street(
         rad=rad, latitude=latitude, longitude=longitude)
@@ -35,7 +36,8 @@ def get_nearest_street(
     n_iter = 0
     while len(ways) != 1:
         #Increase radius after 10 unsucessful iterartions.
-        if not n_iter % max_iter_before_increased_radius:
+        if n_iter == max_iter_before_increased_radius:
+            n_iter = 0
             radplus += 1000
             rad = (radplus + radmoins) / 2
 
