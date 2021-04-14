4ebafe5bac146428799b4549eac459a2377f5fe2
Simon Tronchi <simon.tronchi@student-cs.fr>
2021-04-14 11:27:07 +0200

    add results visualisation

--- package/utils/utils.py
+++ package/utils/utils.py
@@ -7,6 +7,12 @@ from typing import Dict, List, Tuple
 from numpy import array, cross, dot
 from numpy.linalg import norm
 
+import folium
+from branca.element import Figure
+
+import pandas as pd
+from bs4 import BeautifulSoup
+
 from ..API.get_nearest_street import get_nearest_street
 from ..API.get_ways_from_node import get_ways_from_node
 
@@ -132,3 +138,81 @@ def find_optimal(
     """
     key_min = min(distances_dict.items(), key=itemgetter(1))[0]
     return key_min
+
+def visualisation_sections(
+    list_data: List[Tuple],
+    map_filename: str,
+) -> None:
+    """Save the HTML code of the map visualisation. Every tuple correspond to a different point.
+    :param list_data: list of tuples which contains
+            in position 0: coordinates of the point as a tuple (latitude,longitude),
+            in position 1: section of the road related with the point (outpput of find_optimal) with the type List['str','str',tuple,tuple] as the output of get_road_section,
+            in position 2: list of all the node of a street (output of get_ways_from_node).
+    :param map_filename: name of the map HTML code file.
+    return None: the function just save the code with the correct path.  
+    """
+    
+    figure = Figure(height=550,width=750)
+    map_display = folium.Map(location=[48.896205, 2.260466],tiles='cartodbpositron',zoom_start=14)
+    figure.add_child(map_display)
+    colors = ['red', 'blue', 'green', 'purple', 'orange', 'darkred', 'lightred', 'beige', 'darkblue', 'darkgreen', 'cadetblue', 'darkpurple', 'white', 'pink', 'lightblue', 'lightgreen', 'gray', 'black', 'lightgray']
+    color_index = 0
+    for data in list_data:
+        latitude, longitude = data[0]
+        section = data[1]
+        list_node_street = data[2]
+        begin_coordinates = section[2]
+        end_coordinates = section[3]
+        coordinates = [node[1] for node in list_node_street]
+        coordinates = coordinates[coordinates.index(begin_coordinates):coordinates.index(end_coordinates)+1]
+        feature_group = folium.FeatureGroup(f"Tronçon Point ({latitude}, {longitude})")
+        folium.vector_layers.PolyLine(coordinates, popup=f'<b>Tronçon Point ({latitude}, {longitude})</b>', tooltip=f'Tronçon Point ({latitude}, {longitude})', color=colors[color_index%len(colors)], weight=10).add_to(feature_group)
+        folium.Marker(location=[latitude,longitude], popup='Custom Marker 1', tooltip=f'<strong>Point d\'intérêt ({latitude}, {longitude}) </strong>', icon=folium.Icon(color=colors[color_index%len(colors)], icon='none')).add_to(map_display)
+        color_index += 1
+        feature_group.add_to(map_display)
+    folium.LayerControl().add_to(map_display)
+    map_display.save(map_filename)
+    return None
+
+def generate_results(
+    results_dataframe: pd.core.frame.DataFrame,
+    layout_filename: str,
+    map_filename: str,
+    results_filename: str,
+) -> None:
+    """ Save the HTML file to display the results with the website theme.
+
+    :param results_dataframe: Output of the pipeline as a pandas DataFrame.
+    :param layout_filename: Path to get the layout HTML code file.
+    :param map_filename: Path to get the map HTML code file.
+    :param results_filename: Path to save the results HTML code file.
+
+    return None: the function just save the HTML code in a file.
+    """
+    layout = open(layout_filename, 'r')
+    html_layout = layout.read()
+    soup_layout = BeautifulSoup(html_layout)
+    head_layout = soup_layout.find('head')
+    footer_layout = soup_layout.find('footer')
+    navigation = soup_layout.find('nav')
+    scripts_layout = soup_layout.findAll('script')
+    map_display = open(map_filename, 'r')
+    html_map = map_display.read()
+    soup_map = BeautifulSoup(html_map)
+    head_map = soup_map.find('head')
+    body_map = soup_map.find('body')
+    scripts_map = soup_map.findAll('script')
+    dataframe_html = results_dataframe.to_html()
+    
+    head = str(head_layout)[:-7]+str(head_map)[6:]
+    body = '<body>'+str(navigation)+'<div class="row" id = "div_map">'+str(body_map)[6:-7]+'</div>'+dataframe_html+'</div>'+str(footer_layout)+'</body>'
+    
+    message = '<html>'+head+body
+    f = open(results_filename, 'w')
+    f.write(message)
+    for script in scripts_layout:
+        f.write(str(script))
+    for script in scripts_map:
+        f.write(str(script))
+    f.close()
+    return None
